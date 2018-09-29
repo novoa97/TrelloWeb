3e8a65f27fd1a70e43148c965c094e218887f9a9
Eduard <eduardbm999@gmail.com>
2018-09-29 05:28:09 +0200

    router files update

--- server/router.js
+++ server/router.js
@@ -1,14 +1,34 @@
 var express = require('express')
 var router = express.Router()
+var http = require('http'),
+    qs = require('querystring');
 
-//var controlador = require('./server/controller.js')
+var controlador = require('./controller.js')
 
-
-router.get('/', function(req,res){
-  res.send("Hola navegador")
+router.get("/newcard", function(req, res){
+  res.sendFile("newCard.html")
+} )
+router.get("/login", function(req, res){
+  res.sendFile("login.html")
+} )
+router.post("/newcard", function (req, res){
+  var title = req.body.title
+  var desc = req.body.desc
+  var priority = req.body.priority
+  controller.newCard(title, description, priority, function(respuesta){
+    console.log("Recibido")
+    res.send("Holi")
+  })
 })
-router.get('/post',function(req,res){
-  res.send("adios")
+router.post("/login", function (req, res){
+  var user = req.body.title
+  var desc = req.body.desc
+  var priority = req.body.priority
+  controller.newCard(title, description, priority, function(respuesta){
+    console.log("Recibido")
+    res.send("Holi")
+  })
 })
+
 
 module.exports = router
