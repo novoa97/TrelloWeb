8ec1e10fac99c2ff39a25e3d7bd98dafc68ff463
atmarc <marquitus99@gmail.com>
2018-10-25 09:09:35 +0200

    Afegeixo el fitxer mail.js

---
+++ server/mail.js
@@ -0,0 +1,21 @@
+
+
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'marquitus99@gmail.com',
+        pass: ''
+    }
+});
+var mailOptions = {
+    from: 'marquitus99@gmail.com',
+    to: 'marc.amoros.trepat@gmail.com',
+    subject: 'Targeta creada',
+    text: 'La targeta ha sigut creada'
+};
+
+transporter.sendMail(mailOptions, (err, info) => {
+    if (err) console.log(err);
+    else console.log('Email sent: ' + info.response);
+})
