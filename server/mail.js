e19b1541c79f156519b0bbfc76e695106cf8f502
atmarc <marquitus99@gmail.com>
2018-10-25 18:58:22 +0200

    Marc

--- server/mail.js
+++ server/mail.js
@@ -1,21 +1,27 @@
 
+var nodemailer = require('nodemailer'); 
+// email sender function 
+exports.sendEmail = function(req, res){
+// nodemailer stuff will go here
 
-var nodemailer = require('nodemailer');
-var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'marquitus99@gmail.com',
-        pass: ''
-    }
-});
-var mailOptions = {
-    from: 'marquitus99@gmail.com',
-    to: 'marc.amoros.trepat@gmail.com',
-    subject: 'Targeta creada',
-    text: 'La targeta ha sigut creada'
+    var transp1orter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'trello@jediupc.com',
+            pass: ''
+        }
+    });
+    var mailOptions = {
+        from: 'trello@jediupc.com',
+        to: req.correu,
+        subject: 'Targeta creada',
+        text: 'La targeta ha sigut creada'
+    };
+
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) console.log(err);
+        else console.log('Email sent: ' + info.response);
+    })
 };
 
-transporter.sendMail(mailOptions, (err, info) => {
-    if (err) console.log(err);
-    else console.log('Email sent: ' + info.response);
-})
+
