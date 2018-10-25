3f4b25d739fc8b6cee1d5e85e094d996f144ef08
Eduard <eduardbm999@gmail.com>
2018-10-25 19:03:43 +0200

    Coses del login que segurament no funcionin

---
+++ config/auth.js
@@ -0,0 +1,11 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+    'googleAuth' : {
+        'clientID'      : 'your-secret-clientID-here',
+        'clientSecret'  : 'your-client-secret-here',
+        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+    }
+
+};
--- server/router.js
+++ server/router.js
@@ -24,6 +24,49 @@ router.post("/newcard", function (req, res){
     res.send("Holi")
   })
 })
+//COSES DE LOGIN DE GOOGLE
+  // route for showing the profile page
+app.get('/profile', isLoggedIn, function(req, res) {
+    res.render('profile.ejs', {
+        user : req.user // get the user out of session and pass to template
+    });
+});
+
+  // route for logging out
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+    // facebook routes
+    // twitter routes
+
+    // =====================================
+    // GOOGLE ROUTES =======================
+    // =====================================
+    // send to google to do the authentication
+    // profile gets us their basic information including their name
+    // email gets their emails
+app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+
+    // the callback after google has authenticated the user
+app.get('/auth/google/callback',
+        passport.authenticate('google', {
+                successRedirect : '/profile',
+                failureRedirect : '/'
+        }));
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
+
 router.post("/login", function (req, res){
   var user = req.body.title
   var desc = req.body.desc
